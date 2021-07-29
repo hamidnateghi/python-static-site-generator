ae1f751cce375beea5f575d13a2b792724353247
hamidnateghi <88160858+hamidnateghi@users.noreply.github.com>
2021-07-29 17:56:47 -0300

    writing CLI and Site codes
    xd

---
+++ ssg/site.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self):
+        directory = self.dest.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                path.mkdir()
