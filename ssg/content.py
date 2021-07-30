8aa45f57267dcb4a0adcd39e31b94c11b7588b8b
hamidnateghi <88160858+hamidnateghi@users.noreply.github.com>
2021-07-30 16:33:46 -0300

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,31 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = __regex.split(string, 2)
+
+
+load(fm, Loader=FullLoader)
+cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data['content'] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if type in self.data else None
+    
+
