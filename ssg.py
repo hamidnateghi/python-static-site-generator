542336e8368205740fe152e81fde77cb09f784a1
hamidnateghi <88160858+hamidnateghi@users.noreply.github.com>
2021-07-30 17:11:20 -0300

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,17 +1,19 @@
-import ssg.parsers
 import typer
 from ssg.site import Site
 
+import ssg.parsers
 
-def main(source='content', dest='dist'):
-    config = {'source': source,
-              'dest': dest,
-              'parsers': [
-                          ssg.parsers.ResourceParser(),
-                          ssg.parsers.MarkdownParser(),
-                          ssg.parsers.ReStructuredTextParser()
-                         ]
-              }
+
+def main(source="content", dest="dist"):
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
     Site(**config).build()
 
 
