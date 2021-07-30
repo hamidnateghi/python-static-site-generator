37f5876987210ecdfbc15d8396e57dc8829538fe
hamidnateghi <88160858+hamidnateghi@users.noreply.github.com>
2021-07-30 16:39:22 -0300

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -1,20 +1,18 @@
 import re
+
 from yaml import load, FullLoader
 from collections.abc import Mapping
 
 
 class Content(Mapping):
-
     __delimeter = "^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
-        _, fm, content = __regex.split(string, 2)
-
-
-load(fm, Loader=FullLoader)
-cls(metadata, content)
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
@@ -27,5 +25,7 @@ cls(metadata, content)
     @property
     def type(self):
         return self.data["type"] if type in self.data else None
-    
 
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
