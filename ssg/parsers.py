67e14ee2f19f6efad04e91a16a04b58c5d9af35a
hamidnateghi <88160858+hamidnateghi@users.noreply.github.com>
2021-07-30 13:08:35 -0300

    Update

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,20 +10,20 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, source, dest):
+    def parse(self, path=Path, source=Path, dest=Path):
         raise NotImplementedError
 
     def read(self, path):
-        with open(path) as file:
+        with open(file=path) as file:
             return file.read()
 
     def write(self, path, dest, content, ext='.html'):
-        full_path = self.dest / path.with_suffix(ext).name
-        with open(full_path) as file:
+        full_path = dest / path.with_suffix(ext).name
+        with open(file=full_path) as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        copy2(path, dest / path.relative_to(source))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
--- ssg/site.py
+++ ssg/site.py
@@ -17,7 +17,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                Site.run_parser(path)
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -25,9 +25,9 @@ class Site:
                 return parser
 
     def run_parser(self, path):
-        parser = Site.load_parser(path.suffix)
+        parser = self.load_parser(path.suffix)
         if parser is not None:
-            parse()
+            Parser.parse()
         else:
             print('Not Implemented')
 
